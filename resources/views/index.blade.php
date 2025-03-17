f315cb6c3a854456fc5d9b84775edeacb5ef744c
YosepBelfridus <yosep06@gmail.com>
2025-03-17 09:34:38 +0700

    run with vite

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="antialiased">
+    Test HTTPS
+</body>
+
+</html>
